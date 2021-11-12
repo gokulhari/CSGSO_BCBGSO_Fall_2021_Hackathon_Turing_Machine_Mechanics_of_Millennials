39a55842c978438751594df2142c6351d8eb294e
Hamad Ullah <61397521+ullahhab@users.noreply.github.com>
2021-11-12 17:16:47 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import
+
+
+def lstm_to_prep(text):
+    """Passed text and converts this into prepositional logic"""
+    
+
+def correctPrepositional():
+    correct = input("Is this logic correct? type 1 for yes and 0 for no ")
+    #TODO: Print propositional logic that was just converted.
+
+    while correct != '1' and correct != '0':
+        print("invalid argument try again:")
+        correct = input("Is this logic correct? type 1 for yes and 0 for no ")
+    if int(correct) == 0:
+        #assuming we are working with windows
+        osCommandString = "notepad.exe file.txt"
+        os.system(osCommandString)
+        print("Go ahead and edit this file and save it. Press yes when you're done")
+        done = input("Are you done yet? press 1 for yes and 0 for no ")
+        while done != '0' and done != '1':
+            print("invalid argument try again:")
+            done = input("Are you done yet? press 1 for yes and 0 for no ")
+
+
+correctPrepositional()
