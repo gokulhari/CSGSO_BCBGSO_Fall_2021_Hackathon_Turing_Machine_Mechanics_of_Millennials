59adef72ffd8c841bdc33ef0042a34ce54e8fe4f
Gokul Hariharan <gokul@iastate.edu>
2021-11-13 17:02:41 -0600

    Almost done, for parsing to solution

--- GokulFolder/MyeqSolverVisitor.py
+++ GokulFolder/MyeqSolverVisitor.py
@@ -3,22 +3,46 @@ from eqSolverParser import eqSolverParser
 import numpy as np
 import sys
 import math
+import re
 
 
 class MyeqSolverVisitor(eqSolverVisitor):
+    def __init__(self, n = 0, matrix= 1, vector = 2):
+        self.matrix = np.zeros((n,n))
+        self.vector = np.zeros((n,1))
+        self.dic = dict()
+        self.eqnum = 0
+        self.vals = 0
+        self.signs = []
     def visitStatement(self, ctx: eqSolverParser.StatementContext):
         subs = ctx.expr()
-        print(subs[0].getText())
-        print(subs[1].getText())
         subs2 = subs[0].expr()
+        self.vals = float(subs[1].getText())
         children = subs[0].children
         print(children[1].getText())
-        if (children[1].getText() == "+"):
-            
-        return super().visitStatement(ctx)
+        self.signs.append(children[1].getText())
+        print(subs2[0].getText())
+        super().visit(subs2[0])
+        super().visit(subs2[1])
+        return 0
+    
     def visitBuilding_exp(self, ctx: eqSolverParser.Building_expContext):
+        subs = ctx.expr()
+        children = subs[0].children
+        print(children[1].getText())
+        self.signs.append(children[1].getText())
+        super().visit(subs[0])
+        super().visit(subs[1])
+        return 0
+
+    
+    def visitIden_exp(self, ctx: eqSolverParser.Iden_expContext):
+        print(ctx.getText())
+        out = re.split('(\d+)', ctx.getText())
+        while '' in out:
+            out.remove('')
+        print(out)
+        self.dic[out[1]] = float(out[0])
         
-        print(ctx.Sign.getText())
-        if (subs[1].getText() == "+"):
-            a = 2    
-        return super().visitBuilding_exp(ctx)
+            
+        return 0
--- GokulFolder/main.py
+++ GokulFolder/main.py
@@ -19,10 +19,32 @@ def main():
     parser = eqSolverParser(stream)
     tree = parser.program()
     statements = tree.statement()
-    vv = MyeqSolverVisitor()
+    vv = MyeqSolverVisitor(len(statements))
+    n = len(statements)
+    matrix = np.zeros((n,n))
+    vec = np.zeros((n,1))
+    
+    eqno = 0
     for statement in statements:
         vv.visit(statement)
-        print("\n")
+        for key, value in vv.dic.items():
+            if key == "x":
+                matrix[eqno,0] = value        
+            elif key == "y":
+                if vv.signs[0] == "+":
+                    matrix[eqno,1] = value
+                elif vv.signs[0] == "-":
+                    matrix[eqno, 1] = -value
+        vec[eqno] = vv.vals
+        eqno = eqno + 1
+        
+        vv.dict= dict()
+        vv.signs = []
+    print(matrix)
+    print(vec)
+    print("\n")
+    sol = np.linalg.solve(matrix,vec)
+    print(sol)
     
 
 if __name__ == "__main__":
