73c4aa6f9010aaefd34011a64258bb73679f0615
Gokul Hariharan <gokul@iastate.edu>
2021-11-13 17:54:53 -0600

    Final Commit - working code

--- GokulFolder/EquationReaderSolver.py
+++ GokulFolder/EquationReaderSolver.py
@@ -24,30 +24,31 @@ class App:
         height = 500
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height,
-                                    (screenwidth - width) / 2, (screenheight - height) / 2)
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
-
-        GMessage_182 = tk.Message(root)
-        ft = tkFont.Font(family='Times', size=10)
-        GMessage_182["font"] = ft
-        GMessage_182["fg"] = "#333333"
-        GMessage_182["justify"] = "left"
-        GMessage_182["text"] = msg
-        GMessage_182.place(x=120, y=120, width=500, height=100)
-
-        entry = Entry(root, width=50)
-        entry.focus_set()
-        entry.pack()
-
-        ttk.Button(root, text="Done", width=20).pack(pady=20)
+        self.msg = msg
+        L1 = Label(root, text=msg)
+        L1.pack(side = TOP)
+        self.entry = Entry(root)
+        self.entry.focus_set()
+        self.entry.pack()
+        ttk.Button(root, text="Done", command=self.getvalue, width=20).pack(pady=20)
+    def getvalue(self):
+        res = self.entry.get()
+        root.destroy()
+        if res != None:
+            self.ret = res
+        else:
+            self.ret = self.msg
+    
 
 
 if __name__ == "__main__":
-    img = cv2.imread('simpleEq.png')
+    os.chdir("/Users/gokul/Nok/Hackathon2021/CSGSO_BCBGSO_Fall_2021_Hackathon_Turing_Machine_Mechanics_of_Millennials/GokulFolder")
+    img = cv2.imread('simpleEq2.png')
 
-    pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/4.1.1/bin/tesseract'
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'
     # Adding custom options
     custom_config = r'--oem 3 --psm 6'
     result = pytesseract.image_to_string(img, config=custom_config)
